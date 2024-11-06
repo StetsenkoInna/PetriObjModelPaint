d871b9de1fbdf0bec6cc81312f300dde66912b5e
Andrii Kachmar <akacmar1@gmail.com>
2024-11-06 16:06:32 +0200

    crete example for statistic monitor usage

--- src/LibTest/TestPetriObjSimulation.java
+++ src/LibTest/TestPetriObjSimulation.java
@@ -10,19 +10,40 @@ import PetriObj.ExceptionInvalidNetStructure;
 import PetriObj.ExceptionInvalidTimeDelay;
 import PetriObj.PetriObjModel;
 import PetriObj.PetriSim;
+import graphpresentation.statistic.dto.configs.DataCollectionConfigDto;
+import graphpresentation.statistic.dto.data.StatisticConsoleMonitor;
 
 import java.util.ArrayList;
 
 
 /**
- *
  * @author Inna V. Stetsenko
  */
 public class TestPetriObjSimulation {  //Результати співпадають з аналітичними обрахунками
       public static void main(String[] args) throws ExceptionInvalidTimeDelay, ExceptionInvalidNetStructure {
-                   
-     // цей фрагмент для запуску імітації моделі з заданною мережею Петрі на інтервалі часу timeModeling  
+     // цей фрагмент для запуску імітації моделі з заданною мережею Петрі на інтервалі часу timeModeling
           PetriObjModel model = getModel();
+          /**
+           * Example usage of the Statistic module:
+           *
+           * 1) Define the statistic formula to explore:
+           *    1.1) Use functions from the PetriStatFunction enum.
+           *    1.2) Combine functions with mathematical operators.
+           *    1.3) Specify the petri object to take elements from using "O1.". default: 0
+           *
+           * 2) Configure data collection:
+           *    2.1) Set the start time for data collection. default: 0
+           *    2.2) Define the collection step interval. default: 1
+           *
+           * 3) Assign the monitor to the model object.
+           */
+          String formula = "P_AVG(O2.P2)+P_AVG(O3.P2)-T_AVG(O4.T1)";
+          DataCollectionConfigDto dataCollectionConfigDto = new DataCollectionConfigDto();
+          dataCollectionConfigDto.setDataCollectionStep(10000.0);
+          StatisticConsoleMonitor statisticConsoleMonitor = new StatisticConsoleMonitor(formula, dataCollectionConfigDto);
+          statisticConsoleMonitor.setIsMonitoringEnabled(true);
+          model.setStatisticMonitor(statisticConsoleMonitor);
+
           model.setIsProtokol(false);
           double timeModeling = 1000000;
           model.go(timeModeling);
