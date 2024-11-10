69c43909152f0478f9c8bcdaee732265f146155b
account <account@mail.com>
2024-11-10 23:46:42 +0200

    added compile time annotation checks

---
+++ lib-net-annotations/src/main/java/ua/stetsenkoinna/annotation/NetLibraryMethod.java
@@ -0,0 +1,4 @@
+package ua.stetsenkoinna.annotation;
+
+public @interface NetLibraryMethod {
+}
---
+++ petri-obj-paint/src/main/java/ua/stetsenkoinna/graphpresentation/undoable_edits/PasteElementsEdit.java
@@ -0,0 +1,59 @@
+package ua.stetsenkoinna.graphpresentation.undoable_edits;
+
+import ua.stetsenkoinna.PetriObj.ExceptionInvalidNetStructure;
+import ua.stetsenkoinna.graphnet.GraphPetriNet;
+import ua.stetsenkoinna.graphpresentation.GraphElement;
+import ua.stetsenkoinna.graphpresentation.PetriNetsPanel;
+
+import javax.swing.undo.AbstractUndoableEdit;
+
+/**
+ *
+ * @author Leonid
+ */
+public class PasteElementsEdit extends AbstractUndoableEdit {
+    
+    private final PetriNetsPanel panel;
+    
+    /**
+     * Cloned elements and arcs that were pasted
+     */
+    private final GraphPetriNet.GraphNetFragment fragment;
+    
+    public PasteElementsEdit(PetriNetsPanel panel, GraphPetriNet.GraphNetFragment fragment) {
+        this.panel = panel;
+        this.fragment = fragment;
+    }
+    
+    @Override
+    public void undo() {
+        super.undo(); // checking whether it can be undone and setting hasBeenDone = false
+        
+        for (GraphElement element : fragment.elements) {
+            if (element == panel.getCurrent()) {
+                panel.setCurrent(null);
+            }
+            if (element == panel.getChoosen()) {
+               // panel.setChoosen(null);
+            }
+            if (panel.getChoosenElements().contains(element)) {
+                panel.getChoosenElements().remove(element);
+            }
+            try {
+               panel.getGraphNet().delGraphElement(element);
+            } catch (ExceptionInvalidNetStructure e) {
+                e.printStackTrace();
+                // theoretically this exception should never happen here
+            }
+        }
+        panel.repaint();
+    }
+    
+    @Override
+    public void redo() {
+        super.redo();
+        
+        panel.addNetFragment(fragment);
+    }
+    
+}
